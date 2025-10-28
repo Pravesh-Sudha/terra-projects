011ed8b12d6403a22db2528e68af91ff8e48224c
Pravesh-Sudha <programmerpravesh@gmail.com>
2025-10-28 19:05:39 +0530

    Code Added

---
+++ terra-ansible-starter/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e  # exit on error
+
+echo "🚀 Running Terraform Apply..."
+cd terra-config
+terraform apply -auto-approve
+
+terraform output -json instance_public_ips > output.json
+
+
+echo -e "\n📄 Generating Ansible Inventory..."
+cd ..
+python3 scripts/generate_inv.py
+
+
+echo -e "\n🧩 Running Ansible Playbook..."
+cd ansible/
+ansible-playbook playbook.yml
+
+
+echo -e "\n✅ Deployment complete!"
