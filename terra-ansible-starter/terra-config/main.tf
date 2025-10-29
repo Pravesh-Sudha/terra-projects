cf7c3cb31b733c462255268ba08d746a4260c14f
Pravesh-Sudha <programmerpravesh@gmail.com>
2025-10-29 19:30:41 +0530

    main.tf updated

--- terra-ansible-starter/terra-config/main.tf
+++ terra-ansible-starter/terra-config/main.tf
@@ -1,19 +1,7 @@
-# Ec2 Instances
-resource "aws_instance" "tester_1" {
-    
-  for_each = {
-    for inst in local.instances : inst.name => inst.env
-  }
-
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  key_name    = aws_key_pair.tester_key.key_name
-  vpc_security_group_ids = [aws_security_group.tester_sg.id]
-
-  tags = {
-    Name        = each.key
-    Environment = each.value
-  }
+# Key-Pair for Ansible
+resource "aws_key_pair" "tester_key" {
+  key_name   = "tester-key"
+  public_key = file("~/.ssh/appKey.pub")
 }
 
 # Security Group for EC2
@@ -49,8 +37,20 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   ip_protocol       = "-1" 
 }
 
-# Key-Pair for Ansible
-resource "aws_key_pair" "tester_key" {
-  key_name   = "tester-key"
-  public_key = file("~/.ssh/appKey.pub")
+# Ec2 Instances
+resource "aws_instance" "tester_1" {
+    
+  for_each = {
+    for inst in local.instances : inst.name => inst.env
+  }
+
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  key_name    = aws_key_pair.tester_key.key_name
+  vpc_security_group_ids = [aws_security_group.tester_sg.id]
+
+  tags = {
+    Name        = each.key
+    Environment = each.value
+  }
 }
